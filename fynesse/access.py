ca8869da0b5a4f1115ea9ad37b39ff1782d7056a
Keyi Huang <hankenchant@gmail.com>
2024-11-07 04:30:13 +0000

    added create connection function

--- fynesse/access.py
+++ fynesse/access.py
@@ -19,16 +19,41 @@ def data():
 def hello_world():
   print("Hello from the data science library!")
 
-def download_price_paid_data(start_year, end_year):
-   # Base URL where the dataset is stored 
-  base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
-  # File name with placeholders
-  file_name = "/pp-<year>-part<part>.csv"
-  for year in range(start_year, end_year+1):
-    print ("Downloading data for year: " + str(year))
-    for part in range(1,3):
-      url = base_url + file_name.replace("<year>", str(year)).replace("<part>", str(part))
-      response = requests.get(url)
-      if response.status_code == 200:
-        with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
-          file.write(response.content)
+def download_price_paid_data(year_from, year_to):
+    # Base URL where the dataset is stored 
+    base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
+    """Download UK house price data for given year range"""
+    # File name with placeholders
+    file_name = "/pp-<year>-part<part>.csv"
+    for year in range(year_from, (year_to+1)):
+        print (f"Downloading data for year: {year}")
+        for part in range(1,3):
+            url = base_url + file_name.replace("<year>", str(year)).replace("<part>", str(part))
+            response = requests.get(url)
+            if response.status_code == 200:
+                with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
+                    file.write(response.content)
+
+def create_connection(user, password, host, database, port=3306):
+    """ Create a database connection to the MariaDB database
+        specified by the host url and database name.
+    :param user: username
+    :param password: password
+    :param host: host url
+    :param database: database name
+    :param port: port number
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = pymysql.connect(user=user,
+                               passwd=password,
+                               host=host,
+                               port=port,
+                               local_infile=1,
+                               db=database
+                               )
+        print(f"Connection established!")
+    except Exception as e:
+        print(f"Error connecting to the MariaDB Server: {e}")
+    return conn
